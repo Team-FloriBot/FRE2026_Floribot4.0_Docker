856e5b9f4f15f3a457ab6cd73c9fced47c0dac08
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-04-22 16:48:37 +0200

    Update __init__.py

--- gazebo/ros2_ws/src/sim_backend/sim_backend/__init__.py
+++ gazebo/ros2_ws/src/sim_backend/sim_backend/__init__.py
@@ -1,12 +1,1 @@
-        self.front_left_cmd_pub = self.create_publisher(
-            Float64, "/sim/joint_frontLeft/cmd_vel", 10
-        )
-        self.front_right_cmd_pub = self.create_publisher(
-            Float64, "/sim/joint_frontRight/cmd_vel", 10
-        )
-        self.rear_left_cmd_pub = self.create_publisher(
-            Float64, "/sim/joint_rearLeft/cmd_vel", 10
-        )
-        self.rear_right_cmd_pub = self.create_publisher(
-            Float64, "/sim/joint_rearRight/cmd_vel", 10
-        )
+# sim_backend package
