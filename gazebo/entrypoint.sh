7c39c0f5b3b39952de671fe464d4f50690e1f3f2
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-04-23 08:23:29 +0200

    Update entrypoint.sh

--- gazebo/entrypoint.sh
+++ gazebo/entrypoint.sh
@@ -4,9 +4,7 @@ set -e
 source /opt/ros/jazzy/setup.bash
 
 if [ -f /ws/install/setup.bash ]; then
-  source /ws/install/setup.bash
+    source /ws/install/setup.bash
 fi
-
-export GZ_SIM_RESOURCE_PATH=/ws/src/floribot_gz_description:/ws/src/floribot_gz_description/meshes:${GZ_SIM_RESOURCE_PATH}
 
 exec "$@"
